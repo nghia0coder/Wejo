85d8039bd6277d3e1009555fe505149592b7c299
John <nghialmao@gmail.com>
2025-03-12 11:57:09 +0700

    finish set up

---
+++ Wejo/Wejo.Backgroud.Job/Interfaces/ISetting.cs
@@ -0,0 +1,10 @@
+﻿namespace Wejo.Background.Job.Interfaces;
+
+using Common.SeedWork.Interfaces;
+
+/// <summary>
+/// Interface setting
+/// </summary>
+public interface ISetting : ISettingBase
+{
+}
---
+++ Wejo/Wejo.Backgroud.Job/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace Wejo.Background.Job;
+
+using Common.SeedWork;
+using Interfaces;
+
+/// <summary>
+/// Setting
+/// </summary>
+public class Setting : SettingBase, ISetting
+{
+}
