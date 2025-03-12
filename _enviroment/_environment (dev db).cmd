ce9005e0023738fcb139b86c15623dccecfecc8a
John <nghialmao@gmail.com>
2025-03-12 10:45:01 +0700

    WEJO-7: Add backgorund job service

--- _enviroment/_environment (dev db).cmd
+++ _enviroment/_environment (dev db).cmd
@@ -9,3 +9,10 @@ setx Wejo_Ide_SwaggerEnabled "TRUE"
 setx Wejo_Ide_DevMode "TRUE"
 setx Wejo_Ide_Protocols ""
 setx Wejo_Ide_Origins "http://localhost:3000"
+
+
+setx Wejo_Job_Db_Host "34.142.173.238"
+setx Wejo_Job_Db_Port "5432"
+setx Wejo_Job_Db_Name "dev_wejo"
+setx Wejo_Job_Db_UserName "3boyteam"
+setx Wejo_Job_Db_Password "QwerPoiu@2025"
